e9a941c92a85942612240109c78edd693b21edd2
Jenn Smith <smith.jennifer.rchs@gmail.com>
2022-09-09 23:23:24 -0400

    Made comments dynamic and added state

--- PWA_CommentsWebComponent/Components/commentComponent.js
+++ PWA_CommentsWebComponent/Components/commentComponent.js
@@ -1,69 +1,107 @@
-// creating the custom element 
+import { Store } from "./store";
 
-let commentComponent = document.createElement("comment-component")
-commentComponent.setAttribute("name", "")
-commentComponent.setAttribute("email", "")
-commentComponent.setAttribute("comment", "")
-commentComponent.setAttribute("id", "com")
-document.body.append(commentComponent)
+const store = new Store({ comments:[] });
+  console.log(store);
+  // create new store and want to sub to store 
+  // sub to store anytime state changes subscriber is going to call method (adding comment)
+  store.subscribe((state) => {
+    console.log(state)
+    let commentState = state.comments;
+   
+    commentState.forEach(subComment => document.body.appendChild(subComment))
+    
+  });
 
-// creating the template 
-
-const template = document.createElement('template'); 
-
-template.innerHTML = `
-<style></style>
-<div id="comment">
-<p> Name: <span id="names"></span> </p>
-      <p> Email: <span id="emails"></span> </p>
-      <p> Comment: <span id="comments"></span> </p>
-      </div>
-`;
-
-
-
-// append the shadow dom 
 
 class CommentComponent extends HTMLElement {
-
-constructor(){
-    super();
-
-const shadow = this.attachShadow({mode: 'open'}); 
-
-this.shadowRoot.appendChild(template.content.cloneNode(true)); 
-
-shadow.append(template)
-
-this.shadowRoot.querySelector("#name").innerText = this.getAttribute('name')
-this.shadowRoot.querySelector("#email").innerText = this.getAttribute('email')
-this.shadowRoot.querySelector("#comment").innerText = this.getAttribute('comment')
-
-}
-
-
-
-
-}
-
-window.customElements.define('comment-component',CommentComponent);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-export default './commentComponent.js'
+    constructor() {
+      super();
+      this.name = ''
+      this.email = ''
+      this.comment = ''
+  
+      // to init the proper 
+      // used open string later will be changed 
+    }
+  //  look out for attr 
+  // if change then call changed att method
+    static get observedAttributes() {
+      return ["name", "email", "comment"];
+    }
+  // not talking about property but attr 
+  
+  // this method is to know if user adds new value, this method refelcts that value 
+  // refelect the change in the js props 
+  // update the prop to let it know we have the att
+    attributeChangedCallback(attributeName, oldValue, newValue) {
+      if (oldValue === newValue) return;
+      this[attributeName] = newValue; 
+      // updating the empty string with the new value; 
+      // using [] because dont quite know the atrr === this.name , this.email etc 
+  
+      
+    }
+  
+    //  connected callabck is for custom element
+    // add html to build custom element
+    // listeners related to custom element
+    // has to be related to custom element
+    // no btn event listener
+    // btn is not apart of the custom element
+  
+    connectedCallback() {
+      // will be claaed fist time DOM is loaded
+      this.innerHTML = `
+      <p> Name:  ${this.name} </p>
+      <p> email: ${this.email} </p> 
+      <p> comment: ${this.comment} </p>
+      `
+    }
+  }
+  // name.email,comment are properties of JS 
+  // 
+  
+  window.customElements.define("comment-component", CommentComponent);
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.querySelector("button");
+    console.log(btn)
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+  
+      let commentDisplay = document.createElement('comment-component')
+      let name = document.getElementById("name").value
+      let email = document.getElementById("email").value
+      let comment = document.getElementById("comment").value
+  
+      commentDisplay.setAttribute("name", name)
+      commentDisplay.setAttribute("email", email)
+      commentDisplay.setAttribute("comment", comment)
+      // document.body.appendChild(commentDisplay)
+      
+      store.addComment(store.state, commentDisplay);
+    
+    
+     
+    });
+  });
+  
+  // export default CommentComponent
+  export default "./commentComponent.js";
