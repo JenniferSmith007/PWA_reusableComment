33eeb37e786a70b8ac0da52ecd96a122bb06fffc
Jenn Smith <smith.jennifer.rchs@gmail.com>
2022-10-07 15:48:46 -0400

    testing pwa

--- PWA_CommentsWebComponent/main.js
+++ PWA_CommentsWebComponent/main.js
@@ -1,3 +1,11 @@
 import './style.css'
 import formDomSetup from './Components/formDomSetup'
 import commentComponent from './Components/commentComponent'
+
+
+
+
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js');
+  }
