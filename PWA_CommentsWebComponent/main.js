b88025f8c613497ee8f60c83ab92bd3ed6007745
Jenn Smith <smith.jennifer.rchs@gmail.com>
2022-10-07 07:02:38 -0400

    test

--- PWA_CommentsWebComponent/main.js
+++ PWA_CommentsWebComponent/main.js
@@ -2,24 +2,14 @@ import './style.css'
 import formDomSetup from './Components/formDomSetup'
 import commentComponent from './Components/commentComponent'
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js');
+}
 
+// document.querySelector('#show').addEventListener('click', () => {
+//   const iconUrl = document.querySelector('select').selectedOptions[0].value;
+//   let imgElement = document.createElement('img');
+//   imgElement.src = iconUrl;
+//   document.querySelector('#container').appendChild(imgElement);
+// });
 
-import { registerSW } from 'virtual:pwa-register'
-
-const updateSW = registerSW({
-  onNeedRefresh(){
-    console.log('please ref');
-    updateSW()
-  },
-  onOfflineReady(){
-  console.log('offline')
-  },
-  onRegistered(){
-    console.log('regi')
-  },
-  onRegisterError(e){
-    console.log(e)
-    console.log("error")
-  }
-  
-  })
